8451900796069e4f2ad7512be7d1cd0331bf156e
Stanley Lloyd <131160519+StanleyLl0yd@users.noreply.github.com>
2026-08-25 23:59:44 +0300

    Add shared value color unit tests

---
+++ app/src/test/java/com/sl/biorhythms/BiorhythmValueColorTest.kt
@@ -0,0 +1,26 @@
+package com.sl.biorhythms
+
+import org.junit.Assert.assertEquals
+import org.junit.Test
+
+class BiorhythmValueColorTest {
+
+    @Test
+    fun endpointsAndZeroUseExpectedColors() {
+        assertEquals(0xFFFF3B30.toInt(), BiorhythmValueColor.argb(-100.0))
+        assertEquals(0xFFFFCC00.toInt(), BiorhythmValueColor.argb(0.0))
+        assertEquals(0xFF34C759.toInt(), BiorhythmValueColor.argb(100.0))
+    }
+
+    @Test
+    fun valuesOutsideRangeAreClamped() {
+        assertEquals(BiorhythmValueColor.argb(-100.0), BiorhythmValueColor.argb(-500.0))
+        assertEquals(BiorhythmValueColor.argb(100.0), BiorhythmValueColor.argb(500.0))
+    }
+
+    @Test
+    fun midpointColorsAreDeterministic() {
+        assertEquals(0xFFFF8428.toInt(), BiorhythmValueColor.argb(-50.0))
+        assertEquals(0xFF9ADA2D.toInt(), BiorhythmValueColor.argb(50.0))
+    }
+}
