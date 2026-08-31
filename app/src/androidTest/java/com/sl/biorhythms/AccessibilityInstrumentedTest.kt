4bef697151da6e086ef156fff608bb39e3bc7f99
Stanley Lloyd <131160519+StanleyLl0yd@users.noreply.github.com>
2026-08-31 19:27:56 +0300

    Migrate Compose test rule to v2

--- app/src/androidTest/java/com/sl/biorhythms/AccessibilityInstrumentedTest.kt
+++ app/src/androidTest/java/com/sl/biorhythms/AccessibilityInstrumentedTest.kt
@@ -2,7 +2,7 @@ package com.sl.biorhythms
 
 import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.ui.test.hasContentDescription
-import androidx.compose.ui.test.junit4.createComposeRule
+import androidx.compose.ui.test.junit4.v2.createComposeRule
 import androidx.compose.ui.test.onNodeWithContentDescription
 import androidx.compose.ui.test.onNodeWithText
 import com.sl.biorhythms.ui.theme.BiorhythmsTheme
