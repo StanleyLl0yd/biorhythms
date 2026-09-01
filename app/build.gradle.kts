30c283d9e0df4710e40bd7e670dd048b5b7973db
Stanley Lloyd <131160519+StanleyLl0yd@users.noreply.github.com>
2026-09-01 09:51:54 +0300

    Restore required Android test version pin

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -90,6 +90,7 @@ dependencies {
     testImplementation(libs.junit)
 
     androidTestImplementation(libs.androidx.junit)
+    androidTestImplementation(libs.androidx.espresso.core)
     androidTestImplementation(libs.androidx.compose.ui.test.junit4)
 
     debugImplementation(libs.androidx.compose.ui.tooling)
