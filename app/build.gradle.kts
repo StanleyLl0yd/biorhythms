e8da5755c4a7a9dcffa6d7e99aedaa3497312bde
Stanley Lloyd <131160519+StanleyLl0yd@users.noreply.github.com>
2026-09-02 10:13:01 +0200

    Prepare version 1.5.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -22,8 +22,8 @@ android {
         applicationId = "com.sl.biorhythms"
         minSdk = 26
         targetSdk = 37
-        versionCode = 9
-        versionName = "1.4.3"
+        versionCode = 10
+        versionName = "1.5.0"
     }
 
     signingConfigs {
